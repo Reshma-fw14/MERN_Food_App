517f36433bbd6321cb487ca5126748fd7c095729
Reshma-fw14 <reshmaresh9046@gmail.com>
2022-04-02 22:59:53 +0530

    chnages

--- client/src/actions/pizzaAction.jsx
+++ client/src/actions/pizzaAction.jsx
@@ -3,10 +3,10 @@ export const getAllPizzas=()=>dispatch=>{
     dispatch({type:"GET_PIZZASS_REQUEST"})
 
     try{
-        const reqpnse=axios.get('/api/pizzas/getpizzas')
+        const response=axios.get('/api/pizzas/getpizzas')
         console.log(response)
-        dispatch({type:"GET_PIZZASS_REQUEST"})
+        dispatch({type:"GET_PIZZASS_REQUEST", payload:response.data})
     }catch(error){
-        dispatch({type:"GET_PIZZASS_FAILED"})
+        dispatch({type:"GET_PIZZASS_FAILED", payload:error})
     }
 }
