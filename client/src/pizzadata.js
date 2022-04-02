56ae2559e29b9af1b0a9ec5af730237de322694a
Reshma-fw14 <reshmaresh9046@gmail.com>
2022-04-02 15:14:17 +0530

    comit

---
+++ client/src/pizzadata.js
@@ -0,0 +1,16 @@
+[
+  {
+    name: "PEPPER ChICKEN",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+    category: "nonveg",
+    image: "https://images.dominos.co.in/PIZ5158_1.jpg",
+    description: "pepper chicken cheese pizza",
+  },
+];
